f673dccff983ac7c4cbc6b157c3148f631f2a285
KafeOvid <ayushmanng04@gmail.com>
2026-03-14 21:50:42 +0530

    Thread activeNode/setActiveNode props through to Sidebar

--- src/gui/components/ProjectLayout.jsx
+++ src/gui/components/ProjectLayout.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ProjectNavbar from './ProjectNavbar';
 import Sidebar from './Sidebar';
 
-const ProjectLayout = ({ children }) => {
+const ProjectLayout = ({ children, activeNode, setActiveNode }) => {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', width: '100vw', overflow: 'hidden' }}>
             <ProjectNavbar />
             <div style={{ display: 'flex', flexGrow: 1, overflow: 'hidden' }}>
-                <Sidebar />
+                <Sidebar activeNode={activeNode} setActiveNode={setActiveNode} />
                 <div style={{ flexGrow: 1, overflowY: 'auto', backgroundColor: '#16171d' }}>
                     {children}
                 </div>
